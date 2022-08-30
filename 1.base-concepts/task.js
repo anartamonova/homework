2775014f7d54dd51a5efc20cc4e9f45f772b53cd
anartamonova <artangit@yandex.ru>
2022-08-30 21:39:18 +0300

    Task Function added

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,19 @@
+"use strict"
+
 function solveEquation(a, b, c) {
-  let arr;
+  
   // код для задачи №1 писать здесь
+  let arr = [];
+  let diskr = b ** 2 - 4 * a * c;
+  if (diskr > 0) {
+    let x1 = (-b + Math.sqrt(diskr))/(2*a);
+    let x2 = (-b - Math.sqrt(diskr))/(2*a);
+    arr.push(x1,x2);
+    } 
+  else if (diskr === 0) {
+    let x1 = - b /(2 * a);
+    arr.push(x1);
+  } 
   return arr; // array
 }
 
@@ -8,6 +21,41 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
   // код для задачи №2 писать здесь
+ let percentCredit = Number(percent); 
+  let contributionCredit  = Number(contribution); 
+  let amountCredit = Number(amount);
 
+ 
+
+  if (Number.isNaN(percentCredit)){
+    message = `Параметр "Процентная ставка по кредиту" содержит неправильное значение "${percent}"`
+    return message; 
+  }
+  if (Number.isNaN(contributionCredit)){
+    message = `Параметр "Первоначальный взнос" содержит неправильное значение "${contribution}"`;
+    return message; 
+  }
+  if (Number.isNaN(amountCredit)){
+    message = `Параметр "Общая сумма кредита" содержит неправильное значение "${amount}"`; 
+    return message; 
+  }
+
+  let termCredit;
+  let creditSum;
+  let perc;
+  let payment; 
+  let creditTotal;
+  
+  creditSum = amountCredit - contributionCredit;
+  perc = percentCredit / 12/100;
+  
+let dateToday = new Date();
+termCredit = ((date.getFullYear() - dateToday.getFullYear())  * 12 + dateToday.getMonth() - date.getMonth())
+ 
+
+  payment = creditSum * (perc + (perc / (((1 + perc)**termCredit) - 1)));
+  creditTotal = payment * termCredit;
+  totalAmount = Number(creditTotal.toFixed(2));
+ 
   return totalAmount;
 }
--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,75 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
+      min = max = arr[0]; 
+      sum = 0;
+      avg = 0;
 
   // Ваш код
+for (let i = 0; i < arr.length; i++) {
+  if (arr [i] > max) {
+    max = arr[i];}
+    else if (arr[i] < min) {
+      min = arr[i];
+    };
+    sum +=arr[i];
+  };
 
+
+  avg = sum / arr.length;
+  avg = avg.toFixed(2);
+  avg = parseFloat(avg);
+
+  
   return { min: min, max: max, avg: avg };
 }
+
 
 // Задание 2
 function worker(arr) {
   let sum;
-
+  sum = 0;
   // Ваш код
-
+for (let i = 0; i < arr.length; i++) {
+    sum += arr[i]
+  };
   return sum;
 }
-
+console.log(worker ([1, 2, 3]))
 function makeWork(arrOfArr, func) {
   let max;
-
+  max = 0;
   // Ваш кода
   // for ...
+  for (let i=0; i<arrOfArr.length; i++) {
+  let result = func(arrOfArr[i]);
+
+  if (result > max) {
+    max = result;
+  }
+  };
   
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  // Ваш 
+  let max, min, difference;
+    min = max = arr [0];
+  difference = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if(arr[i] < min){
+      min = arr[i];
+    }
+    else if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  difference = Math.abs(max - min);
+
+  return difference;
 }
+
+
