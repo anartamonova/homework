da7e4007e0699f5530a99640c90c152844f58aab
anartamonova <artangit@yandex.ru>
2022-09-01 22:42:25 +0300

    Added HomeWork

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,5 +1,5 @@
 function compareArrays(arr1, arr2) {
-  let result;
+  let result = arr1.length === arr2.length && arr1.every((n,i) => n === arr2[i]);;
 
   // Ваш код
 
@@ -7,7 +7,7 @@ function compareArrays(arr1, arr2) {
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+  let resultArr = arr.filter(item => item > 0).filter(item => item % 3 === 0).map(item => item * 10);;
 
   // Ваш код
 
